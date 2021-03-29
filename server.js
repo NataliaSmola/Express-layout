3b7d25332a132843353e859f69efb6c1f13f6c9b
Natalia <smolutka15@gmail.com>
2021-03-29 22:57:57 +0200

    Update contact form - add image input

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/hello/:name', (req, res) => {
   res.render('hello', {layout: false, name: req.params.name });
 });
@@ -35,7 +39,21 @@ app.get('/history', (req, res) => {
   res.render('history');
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message, image } = req.body;
+  if(author && sender && title && message && image) {
+    res.render('contact', { isSent: true, image: image });
+  }
+  else {
+    if(!image){
+      res.render('contact', { isNoImage: true });
+    }
+    else{
+      res.render('contact', { isError: true, image: image });
+    }
+  }
+});
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
